c1a7a275b132909ea9978cec41b95efca7b5d9f0
sharaafath <36791508+sharaafath@users.noreply.github.com>
2020-03-30 13:13:47 -0500

    Updated IP of QA

--- src/test/java/servlet/cancelpage.java
+++ src/test/java/servlet/cancelpage.java
@@ -17,7 +17,7 @@ public class cancelpage extends HttpServlet {
 				try
 				{
 					Class.forName("org.postgresql.Driver");
-					String jdbcUrl="jdbc:postgresql://54.251.169.42:5432/postgres";
+					String jdbcUrl="jdbc:postgresql://13.58.121.236:5432/postgres";
 					String username="postgres";
 					String password="";
 					
